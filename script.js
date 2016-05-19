3d695e5dafc152aa63792acead186240b21c5886
unknown <hitmachut@tut-14.students.local>
2016-05-19 14:02:26 +0300

    rivka sela 052-7655726

--- script.js
+++ script.js
@@ -1,3 +1,72 @@
+
+$(document).ready(function () {
+    var g;
+    for (var i = 0; i < tree.length; i++) {
+        if (tree[i].gender == 'male')
+            g = '  M';
+        else g = '  F'
+        if (i == 0) {
+            $("#list").append('<li id=' + (i + 1) + '>' + tree[i].title + g + '</li>');
+        }
+
+        $('#' + (i + 1)).append('<ul id=' + i + 1 + '>').addClass("c1");
+        for (var j = 0; j < tree.length; j++) {
+            if (tree[j].gender == 'male')
+                g = ' M';
+            else g = ' F'
+            if (tree[j].parentID == tree[i].id)
+                $('#' + i + 1).append('<li id=' + (j + 1) + '>' + tree[j].title + g + '</li>').addClass("c1");
+        }
+    }
+    $("#list").append(+ +'</ul>')
+
+    });
+
+
+//$(document).ready(function () {
+//    var x = 0;
+//    var pa = 100;
+//    for (var i = 0; i < tree.length; i++) {
+//        //find the parent id
+//        if (tree[i].id < pa)
+//            pa = tree[i].id;
+//    }
+//    var obj;
+//    for (var i = 0; i < tree.length; i++) {
+//        if (tree[i].id == pa)
+//            obj = tree[i];//the first obj ifrom the json
+//    }
+//    var myDiv= $("#list").text(obj.title).append('<ul id=' + x+ '>');
+//    myDiv.append('<li id=' + pa + '>' + tree[pa].title + '</li>');
+   
+//    var first = $("#list").append('<li id=' + (x + 1) + '>' + tree[x].title + g + '</li>');
+//    makeElements(pa,tree)
+//    $("#list").append(+ +'</ul>') 
+//});
+
+
+
+//var myDiv = d3.select('#list')
+//.append("ul")//root ul
+//.append("li")//root li
+//.text(tree.title)//root li name
+//.append("ul");//to this add ul
+
+////note i am iterating through the data via recursion.
+//function makeElements(parentDOM, myData) {
+//    myData.children.forEach(function (child) {
+//        //add li element
+//        parentDOM.append("li").text(child.title);
+//        //if children then make ul
+//        if (child.children.length > 0) {
+//            var ul = parentDOM.append("ul");
+//            //recurse pass ul as parentDOM
+
+//            makeElements(ul, child);
+//        }
+//    });
+//}
+
 var tree = [
 {
 	id : 1,
@@ -173,3 +242,5 @@ var tree = [
 	parentID: 2,
 	gender: "male"
 }];
+
+
