5e535bd38f328a0f4fc2324c16bb0ef7179e1e98
unknown <hitmachut@tut-10.students.local>
2016-05-19 19:16:12 +0300

    Miri Rot 035790439

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
-var tree = [
+var data = [
 {
-	id : 1,
-	title: "Alex",
-	parentID: 0,
-	gender: "male"
+    id: 1,
+    title: "Alex",
+    parentID: 0,
+    gender: "male"
+   
 },
 {
-	id : 2,
-	title: "Avi",
-	parentID: 1,
-	gender: "male"
+    id: 2,
+    title: "Avi",
+    parentID: 1,
+    gender: "male"
+   
+
+
 },
 {
 	id : 3,
@@ -172,4 +176,74 @@ var tree = [
 	title: "Tom",
 	parentID: 2,
 	gender: "male"
-}];
+}
+];
+
+
+function bsd() {
+    var idToNodeMap = {}; //Keeps track of nodes using id as key, for fast lookup
+    var root = null; //Initially set our loop to null
+
+    //loop over data
+    for (var i = 0; i < data.length; i++) {
+        var datum = data[i];
+
+        //each node will have children, so let's give it a "children" poperty
+        //datum["children"] = {};
+        datum.children = [];
+        //add an entry for this node to the map so that any future children can
+        //lookup the parent
+        idToNodeMap[datum.id] = datum;
+
+        //Does this node have a parent?
+        if (datum.parentID == 0)
+        {
+            //Doesn't look like it, so this node is the root of the tree
+            root = datum;
+        }
+        else {
+            //This node has a parent, so let's look it up using the id
+            parentNode = idToNodeMap[datum.parentID];
+
+            //We don't need this property, so let's delete it.
+            delete datum.parent;
+
+            //Let's add the current node as a child of the parent node.
+            parentNode.children.push(datum);
+            
+        }
+       
+    }
+    document.getElementById("list").innerHTML = root.title;
+    d(root);
+    document.getElementById("list").innerHTML+="         "+ root.children[0].title;
+    function d(a)
+    {
+        for (j = 0; j < a.children.length; j++)
+        {
+            document.getElementById("list").append('<li>' + a.children[j].title + '</li>');
+            if(a.children[j].children.length!=0)
+                d(a.children[j])
+        }
+       
+
+    }
+        
+    
+   
+    //for (var i = 0; i < root.length; i++)
+    //{
+    //    var datum = root[i];
+    //    document.getElementById("div1").innerHTML += datum.title+"      ";
+    //}
+    //document.getElementById("div1").innerHTML = JSONTree.create(root);;
+}
+
+function g() {
+    document.getElementById("div1").innerHTML = data;
+   
+}
+function fg(root_tree)
+{
+   
+}
